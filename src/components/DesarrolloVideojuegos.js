97d93ad2370e802c788743ae9d6721391009191b
germano <germanovct@gmail.com>
2024-01-25 19:53:49 -0300

    Primer Commit

---
+++ src/components/DesarrolloVideojuegos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DesarrolloVideojuegos = () => {
+  return (
+    <div>
+      <h2>Desarrollo de Videojuegos</h2>
+      <p>¡Bienvenido a nuestra sección de desarrollo de videojuegos!</p>
+    </div>
+  );
+};
+
+export default DesarrolloVideojuegos;
